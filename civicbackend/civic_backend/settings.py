603f76a1b325921c3a9bbde9d950e1de64f95af2
Himanshu Tiwari <himanshutiwarykhrhna@gmail.com>
2026-09-17 02:58:14 +0530

    updated database env

--- civicbackend/civic_backend/settings.py
+++ civicbackend/civic_backend/settings.py
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'civic_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'civic_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
